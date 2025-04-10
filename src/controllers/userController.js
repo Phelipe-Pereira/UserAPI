f0d5d527f9e0a73e667d324ffad88a732f69a2c9
Phelipe Leandro Pereira <phelipe.pereira0811@hotmail.com>
2025-04-09 21:00:50 -0300

    Criando updateUser

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -43,3 +43,24 @@ async function getUserById(req, res) {
       .json({ erro: "Erro ao buscar usuário", detalhes: err.message });
   }
 }
+
+async function updateUser(req, res) {
+  const userId = req.params.id;
+  const dadosAtualizados = req.body;
+
+  try {
+    const [dadosAlterados] = await User.update(dadosAtualizados, {
+      where: { id: userId },
+    });
+
+    if (dadosAlterados === 0) {
+      return res.status(404).json({ erro: "Usuário não encontrado" });
+    }
+
+    return res.status(200).json({ mensagem: "Usuário atualizado com sucesso" });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ erro: "Erro ao atualizar usuário", detalhes: err.message });
+  }
+}
